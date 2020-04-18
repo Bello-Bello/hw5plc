e8f6e7d03398fe4d0358380d3ca46c96c4447e7e
Bello-Bello <60788906+Bello-Bello@users.noreply.github.com>
2020-04-17 23:00:35 -0400

    Add files via upload

---
+++ equivalences.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+class Student {
+    int age;
+    int idcard;
+    };
+  class Teacher{
+    int age;
+    int idcard;
+    };
+    
+   Student  a, b;
+   Teacher  x, y;
+   
+  int main(){
+      a.age = 23;
+	  a.number = 23456;
+	  printf (“% \n, a.number);
+	  printf (“% \n, x.number);
+	  return 0;
+}
